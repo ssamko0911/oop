132fa2ebb524089098d93b31af5b5e410aa2b4e6
ssamko0911 <ssamko091184@gmail.com>
2025-05-14 12:43:44 +0100

    Add HealthCheckController

---
+++ src/Controller/HealthCheckController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthCheckController
+{
+    #[Route('/healthcheck', name: 'healthcheck', methods: ['GET'])]
+    public function healthCheck(): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+        ]);
+    }
+}
