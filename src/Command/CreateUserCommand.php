ff57d6806789a41f985e8b4bf82a8573a8fa9ff8
ssamko0911 <ssamko091184@gmail.com>
2025-05-22 21:55:23 +0100

    Add permissions

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:create-user')]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $hasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail('serhii@gmail.com');
+        $user->setPassword($this->hasher->hashPassword($user, '123'));
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln('User created!');
+        return Command::SUCCESS;
+    }
+}
