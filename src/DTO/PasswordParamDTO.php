bf8718e607c4497950cc308cc51ea585274edf1e
ssamko0911 <ssamko091184@gmail.com>
2025-06-05 13:23:30 +0100

    Update PasswordParamDTO

--- src/DTO/PasswordParamDTO.php
+++ src/DTO/PasswordParamDTO.php
@@ -13,6 +13,7 @@ final class PasswordParamDTO
     #[Assert\Type(Types::BOOLEAN)]
     public bool $special_symbols = false;
 
+    #[Assert\Type(Types::INTEGER)]
     #[Assert\Range(
         notInRangeMessage: 'Password must be between {{ min }} - {{ max }} characters long',
         min: 8,
